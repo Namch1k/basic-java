7b64a08b2d6b71af462afcf269591ad82d190740
yan-sheleh <yan@divotek.com>
2024-04-17 01:24:27 +0300

    remade university log

--- src/main/java/test/org/Runner.java
+++ src/main/java/test/org/Runner.java
@@ -1,12 +1,16 @@
 package test.org;
 
-import test.org.models.University;
+import com.google.gson.Gson;
 import test.org.creators.UniversityCreator;
+import test.org.models.University;
 
 public class Runner {
     public static void main(String[] args) {
         UniversityCreator universityCreator = new UniversityCreator();
         University university = universityCreator.createTypicalUniversity();
-        System.out.println(university.toString());
+
+        Gson gson = new Gson();
+        String universityJson = gson.toJson(university);
+        System.out.println(universityJson);
     }
 }
